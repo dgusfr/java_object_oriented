01049ac85e697b1e7e4c84a358f59de6e84f8ef1
dgusfr <diegogustavo.fr@gmail.com>
2025-03-13 10:14:59 -0300

    feat add

--- src/entities/EmployeeRegistration.java
+++ src/entities/EmployeeRegistration.java
@@ -1,20 +1,31 @@
 package entities;
 
 public class EmployeeRegistration {
-  public String name;
-  public String department;
-  public double salary;
-  public String startDate;
-  public String rg;
-  public boolean ativo;
-  public String desligar;
+    public String name;
+    public String department;
+    public double salary;
+    public String startDate;
+    public String rg;
+    public boolean ativo;
 
-  public void increaseSalary(double bonus) {
-    salary = salary + bonus;
-  }
+    public EmployeeRegistration() {
+        this.ativo = true;
+    }
 
-  public void dismissEmployee() {
-    ativo = false;
-    System.out.println("Funcionário desligado com sucesso!");
-  }
+    public void increaseSalary(double bonus) {
+        if (bonus > 0) {
+            salary += bonus;
+        } else {
+            System.out.println("O valor da bonificação deve ser positivo.");
+        }
+    }
+
+    public void dismissEmployee() {
+        if (ativo) {
+            ativo = false;
+            System.out.println("Funcionário desligado com sucesso!");
+        } else {
+            System.out.println("Funcionário já está desligado.");
+        }
+    }
 }
